58c50bf6ab8c4e5587071e4433e5dea99aba018f
revakrish266 <revathikrishnan266@gmail.com>
2018-05-13 21:23:27 +0530

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,9 @@
+package com.gocards.mail;
+
+/**
+ * Created by Belal on 10/30/2015.
+ */
+public class Config {
+    public static final String EMAIL ="15euit066@skcet.ac.in";
+    public static final String PASSWORD ="meritamolu";
+}
